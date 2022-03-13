1d3b893454939c78a03235a3c679c5fa568cb136
User <janetsje-wm20@student.tarc.edu.my>
2022-03-13 16:29:07 +0800

    Register

---
+++ app/src/main/java/com/example/joballey/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.joballey
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+
+//    fun login() {
+//        Toast.makeText(this, "Clicked",Toast.LENGTH_LONG).show()
+//    }
+//
+//    fun register() {
+//        val intent = Intent(this, RegisterActivity::class.java)
+//        startActivity(intent)
+//    }
+
+}
