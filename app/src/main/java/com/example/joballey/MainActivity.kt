63e05ce0b1a0f16ddc857484dbcd28b0064e8411
User <janetsje-wm20@student.tarc.edu.my>
2022-03-16 16:27:11 +0800

    register spinner built

--- app/src/main/java/com/example/joballey/MainActivity.kt
+++ app/src/main/java/com/example/joballey/MainActivity.kt
@@ -1,30 +1,34 @@
 package com.example.joballey
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+//    var registration : TextView = findViewById(R.id.register)
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        var login : TextView = findViewById(R.id.sign_in)
+        var registration : TextView = findViewById(R.id.register)
+
+        login.setOnClickListener(){
+            Toast.makeText(this, "Clicked",Toast.LENGTH_LONG).show()
+        }
+
+        registration.setOnClickListener(){
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
     }
-
-
-
-
-
-
-//    fun login() {
-//        Toast.makeText(this, "Clicked",Toast.LENGTH_LONG).show()
-//    }
-//
-//    fun register() {
-//        val intent = Intent(this, RegisterActivity::class.java)
-//        startActivity(intent)
-//    }
 
 }
--- app/src/main/java/com/example/joballey/RegisterActivity.kt
+++ app/src/main/java/com/example/joballey/RegisterActivity.kt
@@ -1,44 +1,30 @@
 package com.example.joballey
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 
 class RegisterActivity : AppCompatActivity() {
 
-    //var education: Spinner = findViewById(R.id.spinner_educationLv)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val success : TextView = findViewById(R.id.register_now)
+//        val uploadFile : Button = findViewById(R.id.upload_pdf)
 
-//        ArrayAdapter.createFromResource(
-//            this,
-//            R.array.educationLv,
-//            android.R.layout.simple_spinner_item
-//        ).also { adapter ->
-//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//            education.adapter = adapter
-//        }
-//
-//        education.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-//
-//            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-//
-//            }
-//
-//            override fun onNothingSelected(p0: AdapterView<*>?) {
-//
-//            }
-//
-//
-//        }
+
+        success.setOnClickListener {
+            val intent1 = Intent(this, MainActivity::class.java)
+            startActivity(intent1)
+        }
+
 
 
     }
+
+
+
 }
