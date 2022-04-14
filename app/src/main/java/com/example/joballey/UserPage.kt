9ff0b9b5119b5d5885064a9f873683cd757023d9
lamwaihong123 <101498426+lamwaihong123@users.noreply.github.com>
2022-04-14 22:56:02 +0800

    hi

--- app/src/main/java/com/example/joballey/UserPage.kt
+++ app/src/main/java/com/example/joballey/UserPage.kt
@@ -7,11 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 
 class UserPage : Fragment() {
+    private lateinit var bal: String
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_user_page, container, false)
     }
 
